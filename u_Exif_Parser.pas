a8d444db7b524e3ac12a5a519a96a6f47dde811c
Sergey Vasketsov <vasketsov@mail.ru>
2012-12-05 00:50:31 +0400

    добавлен парсер тайлов для exif по типу NMC

---
+++ u_Exif_Parser.pas
@@ -0,0 +1,318 @@
+unit u_Exif_Parser;
+
+interface
+
+uses
+  Windows,
+  SysUtils,
+  Classes;
+
+function FindExifInJpeg(const AJpegBuffer: Pointer;
+                        const AJpegSize: Cardinal;
+                        const AForGE: Boolean;
+                        const AExifTag: Word;
+                        out AOffset: PByte;
+                        out ASize: DWORD): Boolean; stdcall;
+
+implementation
+
+type
+  // 4.6.2 IFD Structure
+  // The IFD used in this standard consists of:
+  // a 2-byte count (number of fields),
+  // 12-byte field Interoperability arrays,
+  // and 4-byte offset to the next IFD, in conformance with TIFF Rev. 6.0.
+  TIFD_12 = packed record
+    tag: Word;     // Bytes 0-1 Tag
+    type_: Word;      // Bytes 2-3 Type
+    count: DWORD;  // Bytes 4-7 Count
+    offset: DWORD; // Bytes 8-11 Value Offset
+  end;
+  PIFD_12 = ^TIFD_12;
+
+  TIFD_NN = packed record
+    number_of_fields: Word;
+    items: array of TIFD_12;
+    offset_to_next: DWORD;
+  end;
+  PIFD_NN = ^TIFD_NN;
+
+  TPointedMemoryStream = class(TMemoryStream)
+  end;
+
+function FindExifInJpeg(const AJpegBuffer: Pointer;
+                        const AJpegSize: Cardinal;
+                        const AForGE: Boolean;
+                        const AExifTag: Word;
+                        out AOffset: PByte;
+                        out ASize: DWORD): Boolean; stdcall;
+const
+  c_SOI_Size = 1024;
+
+  function _GetNextWord(ASrcPtr: PByte): Word;
+  begin
+    //CopyMemory(@Result, ASrcPtr, sizeof(Result));
+    Result := ASrcPtr^;
+    Result := (Result shl 8);
+    Inc(ASrcPtr);
+    Result := Result + ASrcPtr^;
+  end;
+
+  function _GetNextDWORD(ASrcPtr: PByte): DWORD;
+  begin
+    //CopyMemory(@Result, ASrcPtr, sizeof(Result));
+    Result := ASrcPtr^;
+
+    Result := (Result shl 8);
+    Inc(ASrcPtr);
+    Result := Result + ASrcPtr^;
+
+    Result := (Result shl 8);
+    Inc(ASrcPtr);
+    Result := Result + ASrcPtr^;
+
+    Result := (Result shl 8);
+    Inc(ASrcPtr);
+    Result := Result + ASrcPtr^;
+  end;
+
+  procedure _ReadIFD12(var ASrcPtr: PByte;
+                       p: PIFD_12);
+  begin
+    p^.tag := _GetNextWord(ASrcPtr);
+    Inc(ASrcPtr,2);
+    p^.type_ := _GetNextWord(ASrcPtr);
+    Inc(ASrcPtr,2);
+    p^.count := _GetNextDWORD(ASrcPtr);
+    Inc(ASrcPtr,4);
+    p^.offset := _GetNextDWORD(ASrcPtr);
+    Inc(ASrcPtr,4);
+  end;
+
+  function _FindSection(const ASrcPtr: PByte;
+                        const AByte1, AByte2: Byte;
+                        const AMaxSteps: Word;
+                        out ANewPtr: PByte): Boolean;
+  var i: Word;
+  begin
+    Result:=FALSE;
+    ANewPtr:=ASrcPtr;
+    i:=AMaxSteps;
+    while (i>0) do begin
+      // check
+      if (AByte1=ANewPtr^) then begin
+        Inc(ANewPtr);
+        if (AByte2=ANewPtr^) then begin
+          Inc(ANewPtr);
+          Result:=TRUE;
+          Exit;
+        end;
+      end;
+      // next
+      Inc(ANewPtr);
+      Dec(i);
+    end;
+  end;
+
+var
+  VChkLen, VEndian, V42: Word;
+  VIFD0: DWORD;
+  VSOIPtr, VAPP1Ptr, VPointer: PByte;
+  VIFD_NN: TIFD_NN;
+  VTagFound: Boolean;
+  
+begin
+  Result:=FALSE;
+  AOffset:=nil;
+  ASize:=0;
+
+  if (nil=AJpegBuffer) or (0=AJpegSize) then
+    Exit;
+
+  // test stream - get SOI section
+  if (AJpegSize>=c_SOI_Size) then
+    VChkLen:=c_SOI_Size
+  else
+    VChkLen:=AJpegSize;
+
+  if not _FindSection(AJpegBuffer, $FF, $D8, VChkLen, VSOIPtr) then
+    Exit;
+
+  // get JFIF as $FF $E0
+  // skipped
+
+  if AForGE then begin
+    // get $FF $FE
+    if not _FindSection(VSOIPtr, $FF, $FE, VChkLen, AOffset) then
+      Exit;
+
+    // get size of section
+    VEndian := _GetNextWord(AOffset);
+
+    Inc(AOffset,2);
+
+    // done
+    ASize := VEndian;
+    Inc(Result);
+    
+    Exit;
+  end;
+
+  // get APP1
+  if not _FindSection(VSOIPtr, $FF, $E1, VChkLen, VAPP1Ptr) then
+    Exit;
+
+  VPointer:=VAPP1Ptr;
+  Inc(VPointer,2); // size of APP1
+
+  // check Exif tag
+  VTagFound:=FALSE;
+  if ({'E'}$45=VPointer^) then begin
+    Inc(VPointer);
+    if ({'x'}$78=VPointer^) then begin
+      Inc(VPointer);
+      if ({'i'}$69=VPointer^) then begin
+        Inc(VPointer);
+        if ({'f'}$66=VPointer^) then begin
+          Inc(VPointer);
+          VTagFound:=TRUE;
+        end;
+      end;
+    end;
+  end;
+
+  if (not VTagFound) then
+    Exit;
+
+  // check next zeroes
+  VTagFound:=FALSE;
+  if ($00=VPointer^) then begin
+    Inc(VPointer);
+    if ($00=VPointer^) then begin
+      Inc(VPointer);
+      VTagFound:=TRUE;
+    end;
+  end;
+    
+  if (not VTagFound) then
+    Exit;
+
+  // Attribute information goes here
+  AOffset := VPointer;
+
+  // get (4949.H) (little endian) or "MM" (4D4D.H) (big endian)
+  VEndian := _GetNextWord(VPointer);
+
+  if ($4949<>VEndian) and ($4D4D<>VEndian) then
+    Exit;
+
+  Inc(VPointer, 2);
+
+  // get 42 (2 bytes) = 002A.H (fixed)
+  V42 := _GetNextWord(VPointer);
+
+  if ($002A<>V42) then
+    Exit;
+
+  Inc(VPointer, 2);
+
+  // get 0th IFD offset (4 bytes).
+  // If the TIFF header is followed immediately by the 0th IFD, it is written as 00000008.H.
+  VIFD0 := _GetNextDWORD(VPointer);
+  Inc(VPointer, 4);
+
+  if (VIFD0<>$00000008) then
+    Inc(VPointer,(VIFD0-$00000008));
+
+  // IFD 0th
+  // The IFD used in this standard consists of:
+  // a 2-byte count (number of fields),
+  // 12-byte field Interoperability arrays,
+  // and 4-byte offset to the next IFD, in conformance with TIFF Rev. 6.0.
+  VIFD_NN.number_of_fields := _GetNextWord(VPointer);
+  if (0=VIFD_NN.number_of_fields) then
+    Exit;
+
+  // $00 $02
+  SetLength(VIFD_NN.items, VIFD_NN.number_of_fields);
+  Inc(VPointer, 2);
+
+  // read items
+  // $82 $98 $00 $02 $00 $00 $00 $20 $00 $00 $00 $26
+  // $87 $69 $00 $04 $00 $00 $00 $01 $00 $00 $00 $46
+  for V42 := 0 to VIFD_NN.number_of_fields-1 do begin
+    _ReadIFD12(VPointer, @(VIFD_NN.items[V42]));
+  end;
+
+  // read next IFD offset
+  // $00 $00 $00 $00
+  VIFD_NN.offset_to_next := _GetNextDWORD(VPointer);
+  Inc(VPointer, 4);
+
+  // TODO: check next IFDs in loop
+
+  VTagFound:=FALSE;
+  // loop items for Exif
+  for V42 := 0 to VIFD_NN.number_of_fields-1 do
+  with VIFD_NN.items[V42] do
+  if ($8769=tag) then
+  if ($0004=type_) then begin
+    // Exif IFD Pointer
+    // Tag = 34665 (8769.H)
+    // Type = LONG (treat as UNSIGNED LONG WORD)
+    // Count = 1
+    // Default = none
+
+    // goes to offset = $00 $00 $00 $46
+    VPointer:=AOffset;
+    Inc(VPointer, offset);
+    VTagFound:=TRUE;
+    break;
+  end;
+
+  // no exif
+  if (not VTagFound) then
+    Exit;
+
+  // do it again
+  VIFD_NN.number_of_fields := _GetNextWord(VPointer);
+  if (0=VIFD_NN.number_of_fields) then
+    Exit;
+
+  SetLength(VIFD_NN.items, VIFD_NN.number_of_fields);
+  Inc(VPointer, 2);
+
+  // read items
+  // $00 $01
+  // $92 $86 $00 $07 $00 $00 $10 $EB $00 $00 $00 $54
+  // type = $00 $07
+  // count = $00 $00 $10 $EB
+  // offset = $00 $00 $00 $54
+  // $41 $53 $43 $49 $49 $00 $00 $00 $3C $3F $78 $6D $6C $20 $76 $65 $72 $73 $69 $6F $6E $3D $22 $31 $2E $30 $22 $20 $65 $6E $63 $6F $64 $69
+  //                                 <   ?
+  for V42 := 0 to VIFD_NN.number_of_fields-1 do begin
+    _ReadIFD12(VPointer, @(VIFD_NN.items[V42]));
+  end;
+
+  // loop items for AExifTag ($9286 = UserComment)
+  VTagFound:=FALSE;
+  for V42 := 0 to VIFD_NN.number_of_fields-1 do
+  with VIFD_NN.items[V42] do
+  if (AExifTag=tag) then begin
+    Inc(AOffset, offset+8);
+    VTagFound:=TRUE;
+    break;
+  end;
+
+  if (not VTagFound) then
+    Exit;
+
+  // treat VExifAttr as PAnsiChar
+  ASize := StrLen(PAnsiChar(AOffset));
+  if (0=ASize) then
+    Exit;
+
+  Inc(Result);
+end;
+
+end.
---
+++ u_Tile_Parser.pas
@@ -0,0 +1,345 @@
+unit u_Tile_Parser;
+
+interface
+
+{$include i_DBMS.inc}
+
+uses
+  Windows,
+  SysUtils,
+  Classes,
+  t_ETS_Tiles,
+  t_DBMS_version;
+
+const
+  c_Tile_Parser_None            = 0;
+  c_Tile_Parser_Exif_NMC_Unique = 1;
+  c_Tile_Parser_Exif_NMC_Latest = 2;
+  //c_Tile_Parser_Exif_GE         = 10;
+  //c_Tile_Parser_Exif_DG_Catalog = 20;
+
+function ParseFullyQualifiedDateTime(const ADateTimeValue: String; out AResult: TDateTime): Boolean;
+
+function GetVersionNumberForDateTimeAsZeroDifference(const ADateTime: TDateTime): Integer;
+
+function ParseExifForGE(
+  const AExifBuffer: PByte;
+  const AExifLength: LongWord;
+  var ANeedFindVersion: Boolean;
+  AParsedVersionPtr: PVersionAA
+): Byte;
+
+function ParseExifForNMC(
+  const AExifBuffer: PByte;
+  const AExifLength: LongWord;
+  const ALookupSpecifiedVersion: Boolean;
+  const AVersionForLookup: String;
+  const AUseUniqueTileIdentifier: Boolean;
+  var ANeedFindVersion: Boolean;
+  AParsedVersionPtr: PVersionAA
+): Byte;
+
+implementation
+
+uses
+  StrUtils;
+
+const
+  c_ZeroVersionNumber_DateTime: TDateTime = 36526; // 36526 = '2000-01-01 00:00:00.00000'
+
+  c_digitalglobe_tileIdentifier_b = '<digitalglobe:tileIdentifier>';
+  c_digitalglobe_tileIdentifier_e = '</digitalglobe:tileIdentifier>';
+
+  c_digitalglobe_featureId_b = '<digitalglobe:featureId>';
+
+  c_digitalglobe_FinishedFeature_e = '</digitalglobe:FinishedFeature>';
+
+function ExtractFromTag(
+  const ASource, ATagBegin, ATagEnd: String;
+  const AStartingPos: Integer;
+  out AValue: String;
+  out AEndOfTagPos: Integer
+): Boolean;
+var
+  VPos, VEnd: Integer;
+begin
+  VPos := PosEx(ATagBegin, ASource, AStartingPos);
+  Result := (VPos>0);
+  if Result then begin
+    VEnd := PosEx(ATagEnd, ASource, VPos+Length(ATagBegin));
+    Result := (VEnd>0);
+    if Result then begin
+      // extract
+      AValue := System.Copy(ASource, VPos+Length(ATagBegin), VEnd-VPos-Length(ATagBegin));
+      // set end of tag
+      AEndOfTagPos := VEnd + (Length(ATagEnd));
+    end;
+  end;
+end;
+
+function GetVersionNumberForDateTimeAsZeroDifference(const ADateTime: TDateTime): Integer;
+begin
+  Result := Round((ADateTime-c_ZeroVersionNumber_DateTime)*86400);
+end;
+
+function IsFullyQualifiedDateTimeDelimiter(const ADelim: Char): Boolean; inline;
+begin
+  Result := (ADelim in ['-','_',' ',':']);
+end;
+
+function ParseFullyQualifiedDateTime(const ADateTimeValue: String; out AResult: TDateTime): Boolean;
+var
+  st: TSystemTime;
+  p: Integer;
+begin
+  // значение типа 2011-05-25 08:09:57.696 для NMC
+  // или типа 2011-08-25_18-15-38 для роскосмоса
+  // доли секунд просто отбрасываем, не округляем
+
+  // проверка формата
+  Result := (19<=Length(ADateTimeValue));
+  if Result then begin
+    Result := IsFullyQualifiedDateTimeDelimiter(ADateTimeValue[5]) and
+              IsFullyQualifiedDateTimeDelimiter(ADateTimeValue[8]) and
+              IsFullyQualifiedDateTimeDelimiter(ADateTimeValue[11]) and
+              IsFullyQualifiedDateTimeDelimiter(ADateTimeValue[14]) and
+              IsFullyQualifiedDateTimeDelimiter(ADateTimeValue[17]);
+  end;
+
+  // парсим по очереди
+
+  // year
+  if Result then begin
+    Result := TryStrToInt(System.Copy(ADateTimeValue,1,4), p);
+    if Result then
+      st.wYear := p;
+  end;
+  // month
+  if Result then begin
+    Result := TryStrToInt(System.Copy(ADateTimeValue,6,2), p);
+    if Result then
+      st.wMonth := p;
+  end;
+  // day
+  if Result then begin
+    Result := TryStrToInt(System.Copy(ADateTimeValue,9,2), p);
+    if Result then
+      st.wDay := p;
+  end;
+
+  // hour
+  if Result then begin
+    Result := TryStrToInt(System.Copy(ADateTimeValue,12,2), p);
+    if Result then
+      st.wHour := p;
+  end;
+  // minute
+  if Result then begin
+    Result := TryStrToInt(System.Copy(ADateTimeValue,15,2), p);
+    if Result then
+      st.wMinute := p;
+  end;
+  // second
+  if Result then begin
+    Result := TryStrToInt(System.Copy(ADateTimeValue,18,2), p);
+    if Result then
+      st.wSecond := p;
+  end;
+
+  // окончаловка
+  if Result then begin
+    // others
+    st.wMilliseconds := 0;
+    // convert
+    try
+      AResult := SystemTimeToDateTime(st);
+      // ok
+    except
+      Result := FALSE;
+    end;
+  end;
+end;
+
+procedure ParseLatestXMLtoVersion(
+  const ASource: String;
+  const ALookupSpecifiedVersion: Boolean;
+  const AVersionForLookup: String;
+  AFeatureVersionPtr: PVersionAA
+);
+var
+  VLatestDate, VSingleDate: TDateTime;
+  VExtractedValue: String;
+  VEndOfTagPos: Integer;
+  VComment: String;
+begin
+  (*
+  даты берём из
+  <digitalglobe:acquisitionDate>2011-05-25 08:09:57.696</digitalglobe:acquisitionDate>
+  <digitalglobe:earliestAcquisitionDate>2011-05-25 08:09:55.0</digitalglobe:earliestAcquisitionDate>
+  <digitalglobe:latestAcquisitionDate>2011-05-25 08:09:57.0</digitalglobe:latestAcquisitionDate>
+  *)
+  if ExtractFromTag(ASource, '<digitalglobe:latestAcquisitionDate>', '</digitalglobe:latestAcquisitionDate>', 1, VExtractedValue, VEndOfTagPos) then begin
+    // есть latestAcquisitionDate
+    if not ParseFullyQualifiedDateTime(VExtractedValue, VLatestDate) then
+      VLatestDate := 0;
+  end else begin
+    VLatestDate := 0;
+  end;
+
+  if ExtractFromTag(ASource, '<digitalglobe:acquisitionDate>', '</digitalglobe:acquisitionDate>', 1, VExtractedValue, VEndOfTagPos) then begin
+    // есть acquisitionDate
+    if not ParseFullyQualifiedDateTime(VExtractedValue, VSingleDate) then
+      VSingleDate := 0;
+  end else begin
+    VSingleDate := 0;
+  end;
+
+  // в VLatestDate сложим максимум
+  if (VLatestDate<VSingleDate) then
+    VLatestDate:=VSingleDate;
+  
+  if (VLatestDate>AFeatureVersionPtr^.ver_date) then begin
+    // надо обновлять данные о версии, так как нашли более новую
+    // featureId у нас тут с самого начала строки до первого тэга - его сунем в значение версии
+    VEndOfTagPos := Pos('</digitalglobe:featureId>', ASource);
+    if (VEndOfTagPos>0) then begin
+      // ok
+      VComment := System.Copy(ASource, 1, (VEndOfTagPos-1));
+      // обрабатываем feature только если либо затребованная версия совпала, либо версии не было
+      if (not ALookupSpecifiedVersion) or SameText(AVersionForLookup, VComment) then begin
+        AFeatureVersionPtr^.ver_value  := VComment;
+        AFeatureVersionPtr^.ver_date   := VLatestDate;
+        AFeatureVersionPtr^.ver_number := GetVersionNumberForDateTimeAsZeroDifference(VLatestDate);
+
+        // соберём комментарий к версии
+        // что-то типа "Strip WV02 Pan Sharpened Natural Color 0.50 Meter country_coverage"
+        VComment := '';
+        // Strip
+        // Mosaic Product
+        if ExtractFromTag(ASource, '<digitalglobe:sourceUnit>', '</digitalglobe:sourceUnit>', 1, VExtractedValue, VEndOfTagPos) then
+          VComment := VComment + ',' + VExtractedValue;
+        // WV01
+        // WV02
+        if ExtractFromTag(ASource, '<digitalglobe:source>', '</digitalglobe:source>', 1, VExtractedValue, VEndOfTagPos) then
+          VComment := VComment + ',' + VExtractedValue;
+        // Pan Sharpened Natural Color
+        // Panchromatic
+        if ExtractFromTag(ASource, '<digitalglobe:productType>', '</digitalglobe:productType>', 1, VExtractedValue, VEndOfTagPos) then
+          VComment := VComment + ',' + VExtractedValue;
+        // 0.50 Meter
+        if ExtractFromTag(ASource, '<digitalglobe:groundSampleDistance>', '</digitalglobe:groundSampleDistance>', 1, VExtractedValue, VEndOfTagPos) then begin
+          VComment := VComment + ',' + VExtractedValue;
+          if ExtractFromTag(ASource, '<digitalglobe:groundSampleDistanceUnit>', '</digitalglobe:groundSampleDistanceUnit>', 1, VExtractedValue, VEndOfTagPos) then
+            VComment := VComment + ' ' + VExtractedValue;
+        end;
+        // country_coverage
+        // metro
+        if ExtractFromTag(ASource, '<digitalglobe:dataLayer>', '</digitalglobe:dataLayer>', 1, VExtractedValue, VEndOfTagPos) then
+          VComment := VComment + ',' + VExtractedValue;
+        // готово - удалим первую запятую
+        System.Delete(VComment, 1, 1);
+        AFeatureVersionPtr^.ver_comment := Trim(VComment);
+      end;
+    end;
+  end;
+end;
+
+function ParseExifForGE(
+  const AExifBuffer: PByte;
+  const AExifLength: LongWord;
+  var ANeedFindVersion: Boolean;
+  AParsedVersionPtr: PVersionAA
+): Byte;
+var
+  VText: String;
+begin
+  SetString(VText, PChar(AExifBuffer), AExifLength);
+  if (0=Length(VText)) then begin
+    Result := ETS_RESULT_INVALID_EXIF;
+    Exit;
+  end;
+
+  Result := ETS_RESULT_OK;
+end;
+
+function ParseExifForNMC(
+  const AExifBuffer: PByte;
+  const AExifLength: LongWord;
+  const ALookupSpecifiedVersion: Boolean;
+  const AVersionForLookup: String;
+  const AUseUniqueTileIdentifier: Boolean;
+  var ANeedFindVersion: Boolean;
+  AParsedVersionPtr: PVersionAA
+): Byte;
+var
+  VXML, VUniqueTileIdentifier: String;
+  VPos, VEndOfTagPos: Integer;
+  VTagValue: String;
+  VFeatureVersion: TVersionAA;
+begin
+  SetString(VXML, PChar(AExifBuffer), AExifLength);
+  if (0=Length(VXML)) then begin
+    Result := ETS_RESULT_INVALID_EXIF;
+    Exit;
+  end;
+
+  // текст до digitalglobe:tileIdentifier нас не интересует в принципе
+  VPos := System.Pos(c_digitalglobe_tileIdentifier_b, VXML);
+  if (VPos>0) then begin
+    System.Delete(VXML, 1, (VPos-1));
+  end;
+
+  VUniqueTileIdentifier := '';
+
+  if AUseUniqueTileIdentifier then begin
+    // уникальный идентификатор версии тайла сидит внутри
+    // <digitalglobe:tileIdentifier>f3c948fe359237bfa0c0801f0aa91c8f</digitalglobe:tileIdentifier>
+    // причём он может как совпадать с идентификатором feature, так и отличаться от них всех
+    if not ExtractFromTag(VXML, c_digitalglobe_tileIdentifier_b, c_digitalglobe_tileIdentifier_e, 1, VTagValue, VEndOfTagPos) then begin
+      // не смогли определить tileIdentifier
+      Result := ETS_RESULT_INVALID_EXIF;
+      Exit;
+    end;
+    // успешно определили tileIdentifier
+    VUniqueTileIdentifier := VTagValue;
+  end;
+
+
+  // а теперь работаем по списку features
+  // ищем самое последнее значение AcquisitionDate
+  // если снаружи фильтруемся по конкретной версии - значит смотрим только её
+  VFeatureVersion.Clear;
+  VPos := 1;
+  while ExtractFromTag(VXML, c_digitalglobe_featureId_b, c_digitalglobe_FinishedFeature_e, VPos, VTagValue, VEndOfTagPos) do begin
+    // парсим кусок XML в версию
+    // сразу же проверяем что дата более новая
+    ParseLatestXMLtoVersion(
+      VTagValue,
+      ALookupSpecifiedVersion,
+      AVersionForLookup,
+      @VFeatureVersion
+    );
+    // next
+    VPos := VEndOfTagPos;
+  end;
+
+  if (0=Length(VFeatureVersion.ver_value)) then begin
+    // версии не нашлись вообще
+    Result := ETS_RESULT_UNKNOWN_EXIF_VERSION;
+    Exit;
+  end;
+
+  // если работаем по уникальному идентификатору версии тайла - пропихнём её
+  if AUseUniqueTileIdentifier then begin
+    VFeatureVersion.ver_value := VUniqueTileIdentifier;
+  end;
+
+  // если версия сменилась - надо искаться
+  ANeedFindVersion := (not SameText(VFeatureVersion.ver_value, AParsedVersionPtr^.ver_value));
+  // копируемся в выходную версию
+  AParsedVersionPtr^ := VFeatureVersion;
+  
+  Result := ETS_RESULT_OK;
+end;
+
+end.
